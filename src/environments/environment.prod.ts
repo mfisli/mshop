6c437300cf31a84bf44349d56d922c6f8729423c
Maks Fisli <mfisli2@gmail.com>
2018-04-08 16:21:59 -0700

    Added firebase related imports. Corrected project.json versions.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyC-37jbdpTlqn3P6EYq_2yg6t0fb3oR4mU',
+    authDomain: 'mshop-67054.firebaseapp.com',
+    databaseURL: 'https://mshop-67054.firebaseio.com',
+    projectId: 'mshop-67054',
+    storageBucket: '',
+    messagingSenderId: '5683735145'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyC-37jbdpTlqn3P6EYq_2yg6t0fb3oR4mU',
+    authDomain: 'mshop-67054.firebaseapp.com',
+    databaseURL: 'https://mshop-67054.firebaseio.com',
+    projectId: 'mshop-67054',
+    storageBucket: '',
+    messagingSenderId: '5683735145'
+  }
 };
