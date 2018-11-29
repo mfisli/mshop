4e20155bd0d99d5d4b8b748841f3dc51a394be5e
Maks Fisli <mfisli2@gmail.com>
2018-11-28 22:30:27 -0800

    Added data table lib. Fixed bug where page refresh would send to wrong url. Preview for product form only shown if title exists.

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -1,19 +1,63 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { DataTableResource } from 'angular-4-data-table';
+import { Subscription } from 'rxjs/Subscription';
+
 import { ProductService } from '../../services/product.service';
+import { Product } from '../../models/product';
 
 @Component({
   selector: 'app-admin-products',
   templateUrl: './admin-products.component.html',
   styleUrls: ['./admin-products.component.css']
 })
-export class AdminProductsComponent implements OnInit {
+export class AdminProductsComponent implements OnInit, OnDestroy {
 
-  products$;
+  products: Product[];
+  subscription: Subscription;
+
+  tableResource: DataTableResource<Product>;
+  items: Product[] = [];
+  itemCount: number;
 
   constructor(private productService: ProductService) { }
 
   ngOnInit() {
-    this.products$ = this.productService.getAll();
+    this.subscription = this.productService.getAll()
+      .subscribe(products => {
+        this.products = products;
+        this.initializedTable(products);
+      });
+  }
+
+  private initializedTable(products: Product[]) {
+    this.tableResource = new DataTableResource(products);
+    this.tableResource.query({ offset: 0})
+      .then(items => this.items = items);
+    this.tableResource.count()
+      .then(count => this.itemCount = count);
+  }
+
+  filter(query: string) {
+    const filteredProducts = query ?
+      this.products.filter(item => item.title.toLowerCase().includes(query.toLowerCase())) :
+      this.products;
+
+    this.initializedTable(filteredProducts);
+  }
+
+  reloadItems(params) {
+    if (!this.tableResource) {
+      return;
+    }
+
+    this.tableResource.query(params)
+      .then(items => this.items = items);
+  }
+
+  ngOnDestroy(): void {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
   }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { RouterModule } from '@angular/router';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule } from '@angular/forms';
 import { CustomFormsModule } from 'ng2-validation';
+import { DataTableModule } from 'angular-4-data-table';
 
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
@@ -47,6 +48,7 @@ import { ProductService } from './services/product.service';
     BrowserModule,
     FormsModule,
     CustomFormsModule,
+    DataTableModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireDatabaseModule,
     AngularFireAuthModule,
